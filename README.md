83ea5b7f1fc40df322537ebfe8a75d2e714c7814
ckrittima <krittimachtchl.02@gmail.com>
2021-05-23 00:41:51 +0700

    add led blink and rewrite readme

--- README.md
+++ README.md
@@ -1,20 +1,33 @@
 # Microprocessor Project
-## Crowd Size Estimation (People Counter)
+
+This project is about Counting people using Raspberry pi 4, picamera and OpenCV. If people are detected by a picamera it will count people around there and send result to https://thingspeak.com/channels/1396043 and if number of detected people greater than 0 it will light the led.
 
 update the Raspberry Pi
+
 > sudo apt-get update
 
 Install the required dependencies
+
 > sudo apt-get install libhdf5-dev
+
 > sudo apt-get install libhdf5-serial-dev
+
 > sudo apt-get install libatlas-base-dev
+
 > sudo apt-get install libjasper-dev
+
 > sudo apt-get install libqtgui4
+
 > sudo apt-get install libqt4-test
 
 Install the OpenCV in Raspberry Pi
+
 > pip3 install opencv-contrib-python==4.1.0.25
 
 Install other Required Packages
+
 > pip3 install imutils
+
 > pip3 install matplotlib
+
+> sudo apt-get install python3-tk
--- peopleCounter.py
+++ peopleCounter.py
@@ -7,51 +7,65 @@ import time
 import base64
 from matplotlib import pyplot as plt
 from urllib.request import urlopen
+import RPi.GPIO as GPIO
 channel_id = 1396043
-WRITE_API  = 'SHD65JWP7245WB1C'
+WRITE_API = 'SHD65JWP7245WB1C'
 BASE_URL = "https://api.thingspeak.com/update?api_key={}".format(WRITE_API)
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 # In[3]:
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
+
+
 def detector(image):
-   image = imutils.resize(image, width=min(400, image.shape[1]))
-   clone = image.copy()
-   rects, weights = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
-   for (x, y, w, h) in rects:
-       cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-   rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-   result = non_max_suppression(rects, probs=None, overlapThresh=0.7)
-   return result
+    image = imutils.resize(image, width=min(400, image.shape[1]))
+    clone = image.copy()
+    rects, weights = hog.detectMultiScale(
+        image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+    for (x, y, w, h) in rects:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+    result = non_max_suppression(rects, probs=None, overlapThresh=0.7)
+    return result
+
+
 def record(sample_time=5):
-   print("recording")
-   camera = cv2.VideoCapture(0)
-   init = time.time()
-   # ubidots sample limit
-   if sample_time < 3:
-       sample_time = 1
-   while(True):
-       print("cap frames")
-       ret, frame = camera.read()
-       frame = imutils.resize(frame, width=min(400, frame.shape[1]))
-       result = detector(frame.copy())
-       result1 = len(result)
-       print (result1)
-       for (xA, yA, xB, yB) in result:
-           cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
-       plt.imshow(frame)
-       plt.show()
-       # sends results
-       if time.time() - init >= sample_time:
-           thingspeakHttp = BASE_URL + "&field1={}".format(result1)
-           print(thingspeakHttp)
-           conn = urlopen(thingspeakHttp)
-           print("sending result")
-           init = time.time()
-   camera.release()
-   cv2.destroyAllWindows()
+    print("recording")
+    camera = cv2.VideoCapture(0)
+    init = time.time()
+    # ubidots sample limit
+    if sample_time < 3:
+        sample_time = 1
+    while(True):
+        print("cap frames")
+        ret, frame = camera.read()
+        frame = imutils.resize(frame, width=min(400, frame.shape[1]))
+        result = detector(frame.copy())
+        result1 = len(result)
+        print(result1)
+        for (xA, yA, xB, yB) in result:
+            cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+        plt.imshow(frame)
+        plt.show()
+        # sends results
+        if time.time() - init >= sample_time:
+            thingspeakHttp = BASE_URL + "&field1={}".format(result1)
+            print(thingspeakHttp)
+            conn = urlopen(thingspeakHttp)
+            print("sending result")
+            init = time.time()
+            if result1 > 0:
+                GPIO.output(8, GPIO.HIGH)
+    camera.release()
+    cv2.destroyAllWindows()
 # In[7]:
+
+
 def main():
-   record()
+    record()
+
+
 # In[8]:
 if __name__ == '__main__':
-   main()
+    main()
